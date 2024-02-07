f4c1d60e6c4895f5a71673e084955112dc2e0ea0
KlvnWr <kelvin.wartanabe@gmail.com>
2024-02-07 07:04:42 -0300

    First commit

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import logo from "./assets/Logo.svg";
+
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="nlw expert" />
+
+      <form className="w-full">
+        <input 
+          type="text" 
+          placeholder="Busque em suas notas..." 
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500"/>
+      </form>
+
+      <div className="h-px bg-slate-700"/>
+
+      <div className="grid grid-cols-3 auto-rows-[250px]">
+        <div className="bg-slate-50"></div>
+      </div>
+</div>
+  )
+}
